acafe0cc72d841dcab98f84f58fcee938bc08c05
rafacabrera7 <rafael.cabrera@urosario.com>
2021-10-08 11:38:22 -0500

    .

--- Web_Scraping/web_scraping.py
+++ Web_Scraping/web_scraping.py
@@ -4,5 +4,5 @@ import requests
 html_text = requests.get('https://arbetsformedlingen.se/platsbanken/annonser?p=5:tPox_ie4_X9X&l=2:CifL_Rzy_Mku').text
 soup = BeautifulSoup(html_text, 'lxml')
 # print(soup)
-jobs = soup.find(id="_ngcontent-jmq-c94")
+jobs = soup.find('div', class_='card-container')
 print(jobs)
